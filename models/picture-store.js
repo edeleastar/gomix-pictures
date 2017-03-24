cf8f5af48558e381ee8275e46462c9b33b3ca183
Eamonn de Leastar <edeleastar@wit.ie>
2017-03-24 10:25:38 +0000

    first version of picture-store module

---
+++ models/picture-store.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const _ = require('lodash');
+const JsonStore = require('./json-store');
+var fs = require("fs-extra");
+
+const pictureStore = {
+
+  store: new JsonStore('./models/picture-store.json', { pictures: [] }),
+  collection: 'pictures',
+  pictureFolder: 'models/pictures/',
+  path: '',
+
+  init() {
+    fs.ensureDirSync('./' + this.pictureFolder);
+    let fullpath = __dirname.split('/');
+    fullpath.pop();
+    this.path = fullpath.join('/') + '/' + this.pictureFolder;
+  },
+
+  getAlbum(userid) {
+    return this.store.findOneBy(this.collection, { userid: userid });
+  },
+
+  storePicture(userId, imageFile) {
+    if (!this.path) {
+      this.init();
+    }
+    fs.ensureDirSync(this.pictureFolder + userId);
+    const imgFullPath = this.pictureFolder + '/' + userId + '/' + imageFile.name;;
+    imageFile.mv(imgFullPath, function (err) {
+      if (err) {
+        console.log('error saving picture');
+      }
+    });
+  },
+
+  addPicture(userId, title, imageFile) {
+    this.storePicture(userId, imageFile);
+    let album = this.getAlbum(userId);
+    if (!album) {
+      album = {
+        userid: userId,
+        pictures: [],
+      };
+      this.store.add(this.collection, album);
+    }
+
+    const picture = {
+      title: title,
+      img: '/' + userId + '/' + imageFile.name,
+      file: imageFile.name,
+    }
+    album.pictures.push(picture);
+  },
+
+  getPicturePath(id, name) {
+    return this.path + id + '/' + name;
+  },
+};
+
+module.exports = pictureStore;
